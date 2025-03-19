d1b0006c498dfb484e911ee14dd7db7d2663ba2f
huseyinkaraman <hsy.krmn@gmail.com>
2025-03-19 18:54:05 +0300

    refactor: Simplify Elasticsearch connection handling in auth service

--- server/3-auth-service/src/config/elasticSearch.ts
+++ server/3-auth-service/src/config/elasticSearch.ts
@@ -1,34 +1,26 @@
-import { winstonLogger } from "@huseyinkaraman/jobber-shared";
-import { Logger } from "winston";
-import { config } from "@auth/config";
-import { Client } from "@elastic/elasticsearch";
-import { ClusterHealthResponse } from "@elastic/elasticsearch/lib/api/types";
+import { winstonLogger } from '@huseyinkaraman/jobber-shared';
+import { Logger } from 'winston';
+import { config } from '@auth/config';
+import { Client } from '@elastic/elasticsearch';
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
 
-const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayElasticConnection', 'debug');
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'authElasticConnection', 'debug');
 
-class ElasticSearch {
-  private elasticSearchClient: Client;
+export const elasticSearchClient = new Client({
+  node: `${config.ELASTIC_SEARCH_URL}`
+});
 
-  constructor() {
-    this.elasticSearchClient = new Client({
-      node: `${config.ELASTIC_SEARCH_URL}`
-    });
-  }
-
-  public async checkConnection(): Promise<void> {
-    let isConnected: boolean = false;
-    while (!isConnected) {
-      try {
-        log.info('GatewayService Connecting to ElasticSearch');
-        const health: ClusterHealthResponse = await this.elasticSearchClient.cluster.health({});
-        log.info(`GatewayService ElasticSearch health status: ${health.status}`);
-        isConnected = true;
-      } catch (error) {
-        log.error('Connection to ElasticSearch failed, Retrying...');
-        log.log('error', `ElasticSearch checkConnection() method error: `, error);
-      }
+export const checkConnection = async (): Promise<void> => {
+  let isConnected: boolean = false;
+  while (!isConnected) {
+    log.info('AuthService Connecting to ElasticSearch ...');
+    try {
+      const health: ClusterHealthResponse = await elasticSearchClient.cluster.health({});
+      log.info(`AuthService ElasticSearch health status: ${health.status}`);
+      isConnected = true;
+    } catch (error) {
+      log.error('Connection to ElasticSearch failed, Retrying...');
+      log.log('error', `AuthService ElasticSearch checkConnection() method error: `, error);
     }
   }
-}
-
-export const elasticSearch = new ElasticSearch();
+};
